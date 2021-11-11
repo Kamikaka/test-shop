6cb9e3b6c19ef9868e108ae2968f3ad32cefaffa
Anatoly Berezin <Nosorog.08@mail.ru>
2021-11-11 17:17:30 +0700

    first

---
+++ components/Header.vue
@@ -0,0 +1,10 @@
+<template>
+    <header>
+        <div class="container">
+            <p>Какой то текст</p>
+        </div>
+    </header>
+</template>
+<script>
+    export default {}
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,7 @@
+<template>
+  <p>qwe</p>
+</template>
+
+<script>
+export default {}
+</script>
